aec1a4c881fcb8414f76a9b6e587486ddce7f2f5
SerratedSharp <97156524+SerratedSharp@users.noreply.github.com>
2024-08-03 19:16:41 -0400

    guard's

--- WasmBrowser.Recipes.WasmClient/wwwroot/PromisesShim.js
+++ WasmBrowser.Recipes.WasmClient/wwwroot/PromisesShim.js
@@ -1,74 +1,52 @@
+PromisesShim.Wait2Seconds = function () {
+    // This also demonstrates wrapping a callback-based API in a promise to 
+    // make it awaitable.
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(); // Resolve promise after 2 seconds
+        }, 2000);
+    });
+};
 
+// Return a value via resolve in a promise.
+PromisesShim.WaitGetString = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("String From Resolve"); // Return a string via promise
+        }, 500);
+    });
+};
 
-let PromisesShim = {}; //globalThis.PromisesShim || {};
-(function (PromisesShim) {
+PromisesShim.WaitGetDate = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Date('1988-11-24')) // Return a date via promise
+        }, 500);
+    });
+};
 
-    PromisesShim.Wait2Seconds = function () {
-        // This also demonstrates wrapping a callback-based API in a promise to make it awaitable.
-        // This is only appropriate for single-shot operations. Events should be used if a callback might be called multiple times.
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {                
-                resolve();// resolve promise after 2 seconds
-            }, 2000);
-        });
-    };
-    
-    // Returning a value via resolve() in a promise
-    PromisesShim.WaitGetString = function () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve("String From Resolve");// return a string via promise
-            }, 500);
-        });
-    };
+// Demonstrates an awaitable fetch.
+PromisesShim.FetchCurrentUrl = function () {
+    // This method returns the promise returned by .then(*.text())
+    // and .NET awaits the returned promise.
+    return fetch(globalThis.window.location, { method: 'GET' })
+        .then(response => response.text());
+};
 
-    PromisesShim.WaitGetDate = function () {        
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(new Date('1988-11-24'))// return a date via promise
-            }, 500);
-        });
-    };
+// .NET can await JS methods using the async/await JS syntax.
+PromisesShim.AsyncFunction = async function () {
+    await PromisesShim.Wait2Seconds();
+};
 
-    // awaitable fetch()
-    PromisesShim.FetchCurrentUrl = function () {
-        // We return the promise returned from .then()
-        // and .NET can in turn await the returned promise.
-        return fetch(globalThis.window.location, { method: 'GET' })
-            .then(response => response.text());
-    };
-
-    // .NET can await JS functions using the async/await JS syntax:
-    PromisesShim.AsyncFunction = async function () {
-        await PromisesShim.Wait2Seconds();
-    };
-
-    PromisesShim.ConditionalSuccess = function (shouldSucceed) {        
-       
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                if (shouldSucceed)                
-                    resolve();// success
-                else                     
-                    reject("Reject: ShouldSucceed == false");// failure
-            }, 500);
-
-        });
-
-    };
-
-    PromisesShim.WaitGetIntArrayAsObject = function () {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve([1, 2, 3, 4, 5] );
-            }, 500);
-        });
-    };
-
-    PromisesShim.UnwrapJSObjectAsIntArray = function (jsObject) {
-        return jsObject;
-    };
-
-})(PromisesShim);
-
-export { PromisesShim };
+// A Promise.reject can be used to signal failure and is bubbled to .NET code 
+// as a JSException.
+PromisesShim.ConditionalSuccess = function (shouldSucceed) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldSucceed)
+                resolve(); // Success
+            else
+                reject("Reject: ShouldSucceed == false"); // Failure
+        }, 500);
+    });
+};
